f8fdc5718193435e06729c72f6894dfe338975bf
Mischa Tobias <mischa.tobias4b@gmail.com>
2024-12-05 15:30:11 -0600

    tried to fix MIME

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
 
 app.use("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(express.static(path.join(__dirname, "public/index.html")));
 });
 
 app.listen(PORT, () => {
